896f4bb22cfa1828cbb86adf4f2f3ccbb24c36ef
subeen-me <subeen.me@gmail.com>
2021-12-30 19:08:45 +0900

    first

---
+++ src/javaVariable/StringExample.java
@@ -0,0 +1,10 @@
+package javaVariable;
+
+public class StringExample {
+    public static void main(String[] args) {
+        String name = "홍길동";
+        String job = "programmer";
+        System.out.println(name);
+        System.out.println(job);
+    }
+}
