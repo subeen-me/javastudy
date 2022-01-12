169e6f8851e17bf802977c29a579af1c12c84794
subeen-me <subeen.me@gmail.com>
2022-01-12 18:41:41 +0900

    DaoExample

---
+++ src/javaInterface/MySqlDao.java
@@ -0,0 +1,28 @@
+package javaInterface;
+
+public class MySqlDao implements DataAccessObject{
+
+    @Override
+    public void select() {
+        System.out.println("MySqlDB에서 검색");
+
+    }
+
+    @Override
+    public void insert() {
+        System.out.println("MySqlDB에서 삽입");
+
+    }
+
+    @Override
+    public void update() {
+        System.out.println("MySqlDB에서 수정");
+
+    }
+
+    @Override
+    public void delete() {
+        System.out.println("MySqlDB에서 삭제");
+
+    }
+}
