362ab9eb608eec4e4a7ebbf87799b65d2a105f73
subeen-me <subeen.me@gmail.com>
2022-01-12 19:58:11 +0900

    nestedclassstudy

---
+++ src/javaNestedClass/A.java
@@ -0,0 +1,57 @@
+package javaNestedClass;
+
+//바깥 클래스
+class A {
+    A() {
+        System.out.println("A객체가 생성됨");
+    }
+
+    //인스턴스 멤버 클래스
+    class B {
+        B() {
+            System.out.println("B객체가 생성됨");
+        }
+
+        int field1;
+
+        //static int field2;
+        void method1() {
+        }
+        //static void method2() {}
+    }
+
+    static class C {
+        C() {
+            System.out.println("C객체가 생성됨");
+        }
+
+        int field1;
+        static int field2;
+
+        void method() {
+        }
+
+        static void method2() {
+        }
+    }
+
+    void method() {
+        //로컬 클래스
+        class D {
+            D() {
+                System.out.println("D객체가 생성됨");
+            }
+
+            int field1;
+
+            //static int field2;
+            void method1() {
+            }
+
+            //static void method2() {}
+        }
+        D d = new D();
+        d.field1 = 3;
+        d.method1();
+    }
+}
