8d85789b39fbdf90aa787a93e311b33ad4110353
subeen-me <subeen.me@gmail.com>
2022-01-13 17:07:38 +0900

    nestedclassstudy

---
+++ src/javaNestedClass/Outter.java
@@ -0,0 +1,21 @@
+package javaNestedClass;
+
+public class Outter {
+    String field = "Outter-field";
+    void method() {
+        System.out.println("Outter-method");
+    }
+
+    class Nested {
+        String field = "Nested-field";
+        void method() {
+            System.out.println("Nested-field");
+        }
+        void print() {
+            System.out.println(this.field);
+            this.method();
+            System.out.println(Outter.this.field);
+            Outter.this.method();
+        }
+    }
+}
---
+++ src/javaNestedClass/OutterExample.java
@@ -0,0 +1,9 @@
+package javaNestedClass;
+
+public class OutterExample {
+    public static void main(String[] args) {
+        Outter outter = new Outter();
+        Outter.Nested nested = outter.new Nested();
+        nested.print();
+    }
+}
