09cfb223bca11ced1f2989261829e23482a5df19
subeen-me <subeen.me@gmail.com>
2022-01-18 17:31:10 +0900

    ThreadStudy

---
+++ src/javaThread/BeepTask.java
@@ -0,0 +1,17 @@
+package javaThread;
+
+import java.awt.*;
+
+public class BeepTask implements Runnable{
+
+    @Override
+    public void run() {
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        for(int i=0; i<5; i++) {
+            toolkit.beep();
+            try {
+                Thread.sleep(500);
+            } catch (Exception e) {}
+        }
+    }
+}
